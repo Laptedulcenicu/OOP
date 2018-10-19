543ebb9677a857ae1b5b20cb0957a72e2bc92561
Laptedulcenicu <43432500+Laptedulcenicu@users.noreply.github.com>
2018-10-19 17:48:03 +0300

    Lab1

---
+++ University.java
@@ -0,0 +1,22 @@
+package com.nicu.Lab1;
+
+public class University {
+    String name;
+    int foundationYear;
+    Student[] lista;
+    Student s;
+    float avg;
+
+    public void printUniversity() {
+        System.out.println("University name :" + name);
+        System.out.println("Foundation year :" + foundationYear);
+        float sum=0;
+        for(Student s: lista ){
+            s.printStudent();
+            sum+=s.mark;
+        }
+
+        avg = sum/lista.length;
+        System.out.println("Media este: " + avg);
+    }
+}
